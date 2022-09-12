6b0f8db7b7a5e76a1d6affc174fb91212474b594
Adriano Santos <sleipnir@bsd.com.br>
2022-09-12 19:42:31 -0300

    Fix. Incorrect import

--- example/actor.py
+++ example/actor.py
@@ -2,7 +2,6 @@
 Copyright 2022 Eigr.
 Licensed under the Apache License, Version 2.0.
 """
-from curses.ascii import NUL
 from spawn.sdk import ActorEntity, ActorInit, ActorParams
 from dataclasses import dataclass, field
 
@@ -11,7 +10,7 @@ from dataclasses import dataclass, field
 class JoeActor(ActorInit):
 
     def init() -> ActorParams:
-        return ActorParams('joe', NUL, 10000, 30000)
+        return ActorParams('joe', "", 10000, 30000)
 
     entity = ActorEntity(init)
 
